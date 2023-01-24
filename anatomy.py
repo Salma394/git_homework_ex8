2856d410e933121407130310548758ef72f2accc
Salma <salmaibrahim813@gmail.com>
2023-01-24 08:42:57 +0000

    I have added some comments

--- anatomy.py
+++ anatomy.py
@@ -2,8 +2,9 @@
 import sys
 
 # argc is a variable and the len (built in function that checks the length)
-# import sys is a module that can show file path
+# import sys is a module
 argument_count = len(sys.argv)
+# conditional statement. If first condition is not satisfied it will go to the next condition.
 if argument_count > 1:
     print('Too many args')
 
@@ -11,5 +12,6 @@ else:
     where = 'World'
     print("Hello", where)
 
+# This output will be printed regardless of the conditional statements as it is outside the block
 print('Goodbye from' + sys.argv[0])
 
