74d596a7ca5d34832ddacca4bb2a86f666657264
Salma <salmaibrahim813@gmail.com>
2023-01-24 09:00:46 +0000

    more changes

--- anatomy.py
+++ anatomy.py
@@ -5,12 +5,9 @@ import sys
 
 # pythons user system module sys is a sytem
 argument_count = len(sys.argv)
-# argv is an argument vector- vector = a list so in this case a list of arguments
-# argument = an input (something we pass into something)
-# if a microwave was a function the argument would be the food you put in, the time, the power
 print(argument_count)
 
-# argument count is a variable
+# argument count is a variable. It is essential that variable names are understandable.
 
 # sys.argv - these count the number of arguments (items) passed to the command line
 # len is a function which counts the number of items or characters inside the variable
